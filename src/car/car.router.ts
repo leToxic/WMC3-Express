b0a42159837bf0c4da34e794f52ad8fb6170dd3f
Sebastian Plasek <s.plasek41@gmail.com>
2023-03-20 09:46:20 +0100

    19 fertig

--- src/car/car.router.ts
+++ src/car/car.router.ts
@@ -40,6 +40,7 @@ export class CarRouter {
             res.status(201).send();
         })
 
+
         this.router.put("/", (req, res) => {
             const car: Car = req.body;
             const updated: Car | null = this.carService.update(car);
